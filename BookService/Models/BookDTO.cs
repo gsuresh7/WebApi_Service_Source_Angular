eca0a9a4b053b41dd7d746c9b685e4c8ac835b7b
Suresh Garikina <suresh.garikina.ext@proximus.com>
2018-01-22 12:27:31 +0100

    test

--- BookService/Models/BookDTO.cs
+++ BookService/Models/BookDTO.cs
@@ -2,7 +2,7 @@
 namespace BookService.Models
 {
     public class BookDTO
-    {
+    { 
         public int Id { get; set; }
         public string Title { get; set; }
         public string AuthorName { get; set; }
