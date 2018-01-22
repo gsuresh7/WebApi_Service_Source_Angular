37476ace662f072c399ea2512f123f6f790a3d66
Suresh Garikina <suresh.garikina.ext@proximus.com>
2018-01-22 12:20:02 +0100

    first commit

---
+++ BookService/Controllers/HeroesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BookService.Models;
+
+namespace BookService.Controllers
+{
+    public class Heroes : ApiController
+    {
+        private BookServiceContext db = new BookServiceContext();
+
+        // GET: api/Authors
+        public IQueryable<Hero> GetHeroes()
+        {
+            return db.Heroes;
+        }
+
+        // GET: api/Authors/5
+        [ResponseType(typeof(Hero))]
+        public async Task<IHttpActionResult> GetHero(int id)
+        {
+            Hero hero = await db.Heroes.FindAsync(id);
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hero);
+        }
+
+        // PUT: api/Authors/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutHero(int id, Hero hero)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != hero.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(hero).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HeroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Authors
+        [ResponseType(typeof(Hero))]
+        public async Task<IHttpActionResult> PostHero(Hero hero)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Heroes.Add(hero);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = hero.id }, hero);
+        }
+
+        // DELETE: api/Authors/5
+        [ResponseType(typeof(Hero))]
+        public async Task<IHttpActionResult> DeleteHero(int id)
+        {
+            Hero hero = await db.Heroes.FindAsync(id);
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            db.Heroes.Remove(hero);
+            await db.SaveChangesAsync();
+
+            return Ok(hero);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool HeroExists(int id)
+        {
+            return db.Heroes.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ BookService/Models/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class Hero
+    {
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+    }
+}
